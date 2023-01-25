d25a807aa9cb17cdc94cbb8568f750ad74c6dbf4
allentevyaw <atevyaw@gmail.com>
2023-01-24 22:01:20 -0500

    create post changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -61,6 +61,24 @@ const CreatePost = () => {
               handleSupriseMe={handleSurpriseMe}
             />
 
+            <div className='relative bg-gray-50 border border-gray-300
+             text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+             focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+                {form.photo ? (
+                  <img
+                    src={form.photo}
+                    alt={form.prompt}
+                    className='w-full h-full object-contain'
+                  />
+                ) : (
+                  <img
+                  src={preview}
+                  alt='preview'
+                  className='w-full h-full object-contain'
+                  />
+                )}
+            </div>
+
           </div>
         </form>
     </section>
