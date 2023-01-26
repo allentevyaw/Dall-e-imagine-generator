497bc0429b8e988c23b04e264ef0e55ef9ecbe45
allentevyaw <atevyaw@gmail.com>
2023-01-25 21:34:05 -0500

    create post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -95,9 +95,18 @@ const CreatePost = () => {
           <button
             type='button'
             onClick={handleGenerateImage}
+            className='text-white bg-green-700 font-medium
+            rounded-md text-sm w-full sm:w-auto px-5 py-2.5
+            text-center'
           >
               {generatingImg ? 'Generating...' : 'Generate'}
           </button>  
+        </div>
+
+        <div className='mt-10'>
+            <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, 
+            you can share it with others in the community
+            </p>
         </div>
 
         </form>
